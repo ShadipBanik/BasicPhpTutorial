e5d0810087df8b952422d0f5f4c84fa13c75a832
ShadipBanik <shadipbanik000@gmail.com>
2018-05-11 00:04:00 +0600

    add screenshot

--- user_management/add_qus.php
+++ user_management/add_qus.php
@@ -4,7 +4,7 @@
       <title></title>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
 
 </head>
 <body>
@@ -55,8 +55,7 @@
 </div>
 </div>
 </body>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 <script src="../resources/js/qus.js"></script>
 </body>
 </html>
